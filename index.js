e69c71b7a452b47e4513f0a704eca3d9b2770406
Nikita Menshutin <nikita.menshutin@ns-mediagroup.com>
2021-09-06 14:34:01 +0300

    used https://gist.github.com/mikberg/0a73b06533927fe717ba

---
+++ index.js
@@ -0,0 +1,3 @@
+"use strict";
+var mongo = require('./modules/db');
+//# sourceMappingURL=index.js.map
---
+++ modules/db.js
@@ -0,0 +1,83 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getCollection = void 0;
+var MongoClient = require('mongodb').MongoClient;
+var promisify = require('es6-promisify').promisify;
+var _connection;
+console.log(process.env);
+var connect = function () {
+    if (!process.env.MONGO_CONNECTION_STRING) {
+        throw new Error("Environment variable MONGO_CONNECTION_STRING must be set to use API.");
+    }
+    return promisify(MongoClient.connect)(process.env.MONGO_CONNECTION_STRING);
+};
+/**
+ * Returns a promise of a `db` object. Subsequent calls to this function returns
+ * the **same** promise, so it can be called any number of times without setting
+ * up a new connection every time.
+ */
+var connection = function () {
+    if (!_connection) {
+        _connection = connect();
+    }
+    return _connection;
+};
+exports.default = connection;
+/**
+ * Returns a ready-to-use `collection` object from MongoDB.
+ *
+ * Usage:
+ *
+ *   (await getCollection('users')).find().toArray().then( ... )
+ */
+function getCollection(collectionName) {
+    return __awaiter(this, void 0, void 0, function () {
+        var db;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, connection()];
+                case 1:
+                    db = _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+exports.getCollection = getCollection;
+//# sourceMappingURL=db.js.map
